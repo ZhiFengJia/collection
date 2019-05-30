7435862c9218ec2bef386ea27452d0d621c7cb23
Jia ZhiFeng <312290710@qq.com>
2019-05-30 17:54:32 +0800

    add-->MyArrayBlockingQueue(自定义阻塞队列,用ArrayList + ReentrantLock实现)

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-JDK集合框架体系
+##JDK集合框架体系
+
+1. MyArrayBlockingQueue(自定义阻塞队列,用ArrayList + ReentrantLock实现)
---
+++ src/main/java/com/jzf/MyArrayBlockingQueue.java
@@ -0,0 +1,107 @@
+package com.jzf;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 自定义阻塞队列(FIFO)
+ * 利用ReentrantLock锁实现
+ *
+ * @author Jia ZhiFeng <312290710@qq.com>
+ * @date 2019/5/30 17:46:41
+ */
+public class MyArrayBlockingQueue<T> {
+    Lock lock = new ReentrantLock(true);
+    Condition c = lock.newCondition();
+    Condition p = lock.newCondition();
+    private ArrayList<T> list = new ArrayList();
+    private int size = 0;
+    private int capacity;
+
+    public MyArrayBlockingQueue(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void put(T o) throws InterruptedException {
+        lock.lock();
+        while (size == capacity) {
+            p.await();
+        }
+        list.add(o);
+        size++;
+        c.signalAll();
+        lock.unlock();
+    }
+
+    public T take() throws InterruptedException {
+        lock.lock();
+        while (list.isEmpty()) {
+            c.await();
+        }
+
+        T t = list.remove(0);
+        size--;
+        p.signalAll();
+        lock.unlock();
+        return t;
+    }
+
+    public int size() {
+        lock.lock();
+        try {
+            return size;
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+        return -1;
+    }
+
+    /**
+     * 测试生产者/消费者
+     */
+    public static void main(String[] args) {
+        MyArrayBlockingQueue<String> queue = new MyArrayBlockingQueue<>(5);
+//        LinkedBlockingQueue<String> queue = new LinkedBlockingQueue<>(5);
+
+        //5个消费者
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    String str = null;
+                    try {
+                        str = queue.take();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "消费消息: " + str);
+                }
+            }, "c" + i).start();
+        }
+
+        //5个生产者
+        for (int i = 0; i < 5; i++) {
+            new Thread(() -> {
+                for (int j = 0; j < 100; j++) {
+                    try {
+                        queue.put(System.currentTimeMillis() + "");
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "生产消息");
+                }
+            }, "p" + i).start();
+        }
+
+//        new Thread(() -> {
+//            while (true){
+//                System.out.println(Thread.currentThread().getName() + "监控消息:" + queue.size());
+//            }
+//        }, "m").start();
+
+    }
+
+}
